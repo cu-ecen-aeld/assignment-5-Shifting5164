bb42dfb997198ca286141d9cb969981c3f18998c
user <user@ubuntu.myguest.virtualbox.org>
2024-01-12 13:14:37 +0100

    [ADD] scull inits and ko, works

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -13,7 +13,8 @@ LDD_VERSION = '18d9a1564294ece4b83032a3757254e5c279627c'
 LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-Shifting5164.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
-LDD_MODULE_SUBDIRS = misc-modules/
+LDD_MODULE_SUBDIRS = misc-modules/ scull/
+LDD_MODULE_SUBDIRS += scull/
 
 define LDD_BUILD_CMDS
 endef
@@ -21,6 +22,8 @@ endef
 define LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(kernel-module))
