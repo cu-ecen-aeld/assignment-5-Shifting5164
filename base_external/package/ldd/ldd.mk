9814526dd3d7d18e93c09cab936d713ad026f15d
user <user@ubuntu.myguest.virtualbox.org>
2024-01-12 13:01:33 +0100

    [ADD] ldd, builds, installs and runs

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,28 +1,27 @@
 
 ##############################################################
 #
-# AESD-ASSIGNMENTS
+# LDD-ASSIGNMENT
 #
 ##############################################################
 
 #DONE: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '18d9a1564294ece4b83032a3757254e5c279627c'
+LDD_VERSION = '18d9a1564294ece4b83032a3757254e5c279627c'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-Shifting5164.git'
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-Shifting5164.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = misc-modules/
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules all
-	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/skull all
+define LDD_BUILD_CMDS
 endef
 
-# DONE add your writer, finder and finder-test utilities/scripts to the installation steps below
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	#$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
-	#$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
